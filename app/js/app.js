f32c9a6e7e726383634e110a9f43b6f41a29a48d
Nils Winkler <nils@nilswinkler.com>
2012-08-13 16:47:29 +0200

    Removed static routes in favor of dynamic ones.

--- app/js/app.js
+++ app/js/app.js
@@ -7,9 +7,6 @@ angular.module('angular-dynamic-routing', ['angular-dynamic-routing.services']).
   config(['$routeProvider', function($routeProvider) {
 	globalRouteProvider = $routeProvider;  
 	  
-    $routeProvider.when('/link1', {templateUrl: 'partials/page1.html', controller: PageController});
-    $routeProvider.when('/link2', {templateUrl: 'partials/page2.html', controller: PageController});
-    $routeProvider.when('/link3', {templateUrl: 'partials/page3.html', controller: PageController});
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: HomeController});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
